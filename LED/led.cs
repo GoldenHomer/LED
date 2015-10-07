7ce0dff75208b5148f3b1e1eae5de058c5277f67
Jose Contreras <con40020@LIB274187.ttu.edu>
2015-10-07 13:11:02 -0500

    file

---
+++ LED/led.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.SPOT;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
+using GHIElectronics.NETMF.FEZ;
+
+namespace MFConsoleApplication1
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			OutputPort LED;
+			LED = new OutputPort((Cpu.Pin)FEZ_Pin.Digital.LED, true);
+			
+			Thread.Sleep(Timeout.Infinite);
+		}
+	}
+}
